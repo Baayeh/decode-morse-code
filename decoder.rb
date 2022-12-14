cac75f9893b968533b3f44235d9596651de404e2
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-14 14:06:10 +0000

    decode entire message

--- decoder.rb
+++ decoder.rb
@@ -41,7 +41,22 @@ end
 
 def decode_word(code)
   _code = code.split(" ")
-_code.each { |i|
-  decode_char(i)
+  _code.each { |i|
+    decode_char(i)
 }
+end
+
+
+# decode entire message
+def decode(message)
+  words = message.split("   ")
+  decoded_msg = ""
+  words.each do |word|
+    letters = word.split(" ")
+    letters.each do |letter|
+      decoded_msg += decode_char(letter)
+    end
+    decoded_msg += " "
+  end
+  return decoded_msg.strip
 end
