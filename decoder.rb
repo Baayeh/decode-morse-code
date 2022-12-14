75306f41b71fe3ba2fb111ffd0babacf4f6de8c6
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-14 14:33:21 +0000

    Add linters

--- decoder.rb
+++ decoder.rb
@@ -1,62 +1,35 @@
-# dictionary
-$dictionary = {
-  "A" => ".-",
-  "B" => "-...",
-  "C" => "-.-.",
-  "D" => "-..",
-  "E" => ".",
-  "F" => "..-.",
-  "G" => "--.",
-  "H" => "....",
-  "I" => "..",
-  "J" => ".---",
-  "K" => "-.-",
-  "L" => ".-..",
-  "M" => "--",
-  "N" => "-.",
-  "O" => "---",
-  "P" => ".--.",
-  "Q" => "--.-",
-  "R" => ".-.",
-  "S" => "...",
-  "T" => "-",
-  "U" => "..-",
-  "V" => "...-",
-  "W" => ".--",
-  "X" => "-..-",
-  "Y" => "-.--",
-  "Z" => "--..",
-  " " => " ",
-}
-
 # decode morse code character
 def decode_char(character)
-  $dictionary.each {
-    |key, value|
-    if character === value
-      return key
-    end
+  # dictionary
+  dictionary = {
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.',
+    'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.', 'Q' => '--.-', 'R' => '.-.', 'S' => '...',
+    'T' => '-', 'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..'
   }
+
+  dictionary.each do |key, value|
+    return key if character == value
+  end
 end
 
-def decode_word(code)
-  _code = code.split(" ")
-  _code.each { |i|
+def decode_word(word)
+  split_word = word.split
+  split_word.each do |i|
     decode_char(i)
-}
+  end
 end
-
 
 # decode entire message
 def decode(message)
-  words = message.split("   ")
-  decoded_msg = ""
+  words = message.split('   ')
+  decoded_msg = ''
   words.each do |word|
-    letters = word.split(" ")
+    letters = word.split
     letters.each do |letter|
       decoded_msg += decode_char(letter)
     end
-    decoded_msg += " "
+    decoded_msg += ' '
   end
-  return decoded_msg.strip
+  decoded_msg.strip
 end
